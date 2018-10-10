7a6a1ac40b418ea866e82aae7a0e844f464f8122
Seamas <seamas.wang@gmail.com>
2018-10-10 15:35:29 +0800

    Upload File Demo

---
+++ UploadFileDemo/Controllers/UploadController.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Features;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.WebUtilities;
+using Microsoft.Extensions.Logging;
+using Microsoft.Net.Http.Headers;
+
+namespace UploadFileDemo.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    public class UploadController : ControllerBase
+    {
+        private readonly ILogger<UploadController> _logger;
+        private static readonly FormOptions _defaultFormOptions = new FormOptions();
+
+        public UploadController(ILogger<UploadController> logger)
+        {
+            _logger = logger;
+        }
+        
+        [HttpPost]
+        public async Task<IActionResult> LargeFile()
+        {
+            if (!MultipartRequestHelper.IsMultipartContentType(Request.ContentType))
+            {
+                return BadRequest($"Expected a multipart request, but got {Request.ContentType}");
+            }
+        
+            // Used to accumulate all the form url encoded key value pairs in the 
+            // request.
+            var formAccumulator = new KeyValueAccumulator();
+            string targetFilePath = null;
+        
+            var boundary = MultipartRequestHelper.GetBoundary(
+                MediaTypeHeaderValue.Parse(Request.ContentType),
+                _defaultFormOptions.MultipartBoundaryLengthLimit);
+            var reader = new MultipartReader(boundary, HttpContext.Request.Body);
+        
+            var section = await reader.ReadNextSectionAsync();
+            while (section != null)
+            {
+                ContentDispositionHeaderValue contentDisposition;
+                var hasContentDispositionHeader = ContentDispositionHeaderValue.TryParse(section.ContentDisposition, out contentDisposition);
+        
+                if (hasContentDispositionHeader)
+                {
+                    if (MultipartRequestHelper.HasFileContentDisposition(contentDisposition))
+                    {
+                        //保存在一个临时文件中
+                        targetFilePath = Path.GetTempFileName();
+                        using (var targetStream = System.IO.File.Create(targetFilePath))
+                        {
+                            await section.Body.CopyToAsync(targetStream);
+        
+                            _logger.LogInformation($"Copied the uploaded file '{targetFilePath}'");
+                        }
+                    }
+                    else if (MultipartRequestHelper.HasFormDataContentDisposition(contentDisposition))
+                    {
+                        // Content-Disposition: form-data; name="key"
+                        //
+                        // value
+        
+                        // Do not limit the key name length here because the 
+                        // multipart headers length limit is already in effect.
+                        var key = HeaderUtilities.RemoveQuotes(contentDisposition.Name).ToString();
+                        var encoding = GetEncoding(section);
+                        using (var streamReader = new StreamReader(
+                            section.Body,
+                            encoding,
+                            detectEncodingFromByteOrderMarks: true,
+                            bufferSize: 1024,
+                            leaveOpen: true))
+                        {
+                            // The value length limit is enforced by MultipartBodyLengthLimit
+                            var value = await streamReader.ReadToEndAsync();
+                            if (String.Equals(value, "undefined", StringComparison.OrdinalIgnoreCase))
+                            {
+                                value = String.Empty;
+                            }
+                            formAccumulator.Append(key, value);
+        
+                            if (formAccumulator.ValueCount > _defaultFormOptions.ValueCountLimit)
+                            {
+                                throw new InvalidDataException($"Form key count limit {_defaultFormOptions.ValueCountLimit} exceeded.");
+                            }
+                        }
+                    }
+                }
+        
+                // Drains any remaining section body that has not been consumed and
+                // reads the headers for the next section.
+                section = await reader.ReadNextSectionAsync();
+            }
+        
+            // Bind form data to a model
+//            var user = new User();
+//            var formValueProvider = new FormValueProvider(
+//                BindingSource.Form,
+//                new FormCollection(formAccumulator.GetResults()),
+//                CultureInfo.CurrentCulture);
+//        
+//            var bindingSuccessful = await TryUpdateModelAsync(user, prefix: "",
+//                valueProvider: formValueProvider);
+//            if (!bindingSuccessful)
+//            {
+//                if (!ModelState.IsValid)
+//                {
+//                    return BadRequest(ModelState);
+//                }
+//            }
+//        
+//            var uploadedData = new UploadedData()
+//            {
+//                Name = user.Name,
+//                Age = user.Age,
+//                Zipcode = user.Zipcode,
+//                FilePath = targetFilePath
+//            };
+            
+            return new JsonResult("hello");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SmallFile(IFormFile file)
+        {
+            if (file == null || file.Length == 0)
+            {
+//                return Content("file not selected");
+                return new JsonResult("file not selected");
+            }
+
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", file.FileName);
+
+            using (var stream = new FileStream(path, FileMode.Create))
+            {
+                await file.CopyToAsync(stream);
+            }
+            
+            return new JsonResult("ok");
+        }
+        
+        private static Encoding GetEncoding(MultipartSection section)
+        {
+            var hasMediaTypeHeader = MediaTypeHeaderValue.TryParse(section.ContentType, out var mediaType);
+            // UTF-7 is insecure and should not be honored. UTF-8 will succeed in 
+            // most cases.
+            if (!hasMediaTypeHeader || Encoding.UTF7.Equals(mediaType.Encoding))
+            {
+                return Encoding.UTF8;
+            }
+            return mediaType.Encoding;
+        }
+        
+    }
+}
